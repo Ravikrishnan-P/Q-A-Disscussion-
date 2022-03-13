7a1135bb206aede7b30c40057416e997a959bb66
Ravikrishnan-P <98149361+Ravikrishnan-P@users.noreply.github.com>
2022-03-13 12:32:40 +0530

    Add files via upload

---
+++ QA_discussion_forum/src/Interface.java
@@ -0,0 +1,30 @@
+
+public interface Interface {
+    class Vehicle {
+    public void startEngine(){
+      System.out.println("This is start Engine");
+    }
+    
+    public void stopEngine(){
+    System.out.println("This is stop Engine");
+    }
+    
+    public void accelarate(){
+    System.out.println("This is accelaration");
+    }
+    
+    public void turn(){
+    System.out.println("This is from turn");
+    }
+    }
+    
+    /*public static void main(String args[])
+    {
+    
+        
+    
+    
+    }*/
+    
+    
+}
---
+++ QA_discussion_forum/src/Vehicles.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hlt
+ */
+public class Vehicles implements Interface {
+    
+    public static void main(String args[]){
+    
+        Vehicle v = new Vehicle();
+        
+        v.accelarate();
+    
+    }
+    
+}
